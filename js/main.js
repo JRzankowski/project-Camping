98fb73091cf9407ae32d3f7a76e72da1c5146fa7
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2019-08-12 22:59:25 +0200

    add stats-section and js

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var stats = document.querySelector('section.stats .stats-item');
+let activate = true;
+let isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+
+let counter = () => {
+
+
+    let firstRanges = [12, 120]
+    let secondRanges = [1280, 1560]
+    const slowerCount = [...document.querySelectorAll(".stats .stats-container .firstCount")]
+    const fasterCount = [...document.querySelectorAll(".stats .stats-container .secondCount")]
+
+    let firstActiveNumber = 0;
+    let secondActiveNumber = 0;
+
+    let firstCountUp = () => {
+
+        for (let i = 0; i <= 1; i++) {
+            setInterval(() => {
+                if (slowerCount[i].textContent < firstRanges[i]) {
+                    firstActiveNumber++
+                    slowerCount[i].textContent = firstActiveNumber
+                } else if (slowerCount[i].textContent = firstRanges[i]) {
+                    return
+
+                }
+            }, 35);
+
+        }
+    }
+    let secondCountUp = () => {
+
+        for (let i = 0; i <= 1; i++) {
+            setInterval(() => {
+                if (fasterCount[i].textContent < secondRanges[i]) {
+                    secondActiveNumber++
+                    fasterCount[i].textContent = secondActiveNumber
+                } else if (fasterCount[i].textContent = secondRanges[i]) {
+                    return
+                }
+            }, 1);
+        }
+    }
+
+    if (isInViewport(stats) && (activate)) {
+        firstCountUp()
+        secondCountUp()
+        activate = !activate
+    }
+
+
+}
+
+window.addEventListener('scroll', counter)
