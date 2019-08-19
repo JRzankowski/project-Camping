2b969c61a64e3cd254d56285b357c775aa16ecfa
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2019-08-19 17:28:36 +0200

    contact section ready

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map;
+var map, infoWindow;
 
 function createMap() {
     var options = {
@@ -6,9 +6,33 @@ function createMap() {
             lat: 43.654,
             lng: -79.383
         },
-        zoom: 10,
-        mapTypeID: google.maps.MapTypeId.HYBRID
+        zoom: 10
     };
-    map = new google.maps.Map(document.getElementById('map'), options);
 
+    map = new google.maps.Map(document.getElementById('map'), options);
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (p) {
+            var position = {
+                lat: p.coords.latitude,
+                lng: p.coords.longitude
+            };
+
+            infoWindow.setPosition(position);
+            infoWindow.setContent('Your location!');
+            infoWindow.open(map);
+            map.setCenter(position);
+        }, function () {
+            handleLocationError('Geolocation service failed', map.getCenter());
+        });
+    } else {
+        handleLocationError('No geolocation available.', map.getCenter());
+    }
+}
+
+function handleLocationError(content, position) {
+    infoWindow.setPosition(position);
+    infoWindow.setContent(content);
+    infoWindow.open(map);
 }
